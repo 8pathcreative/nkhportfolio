aa37cfbcdafec9c13f3672b5ab5a3ce827700750
Neil Humphrey <neilhumphrey@Neils-Laptop.local>
2025-03-22 18:37:51 -0500

    Add grid background to hero section

--- components/hero.tsx
+++ components/hero.tsx
@@ -11,7 +11,14 @@ export default function Hero() {
   }, [])
 
   return (
-    <section className="min-h-screen flex flex-col justify-center items-center pt-16 pb-8 px-0 relative overflow-hidden">
+    <section className="min-h-screen flex flex-col justify-center items-center pt-16 pb-8 px-0 relative overflow-hidden bg-white dark:bg-black">
+      {/* Grid Background */}
+      <div
+        className="absolute inset-0 [background-size:40px_40px] [background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)] dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+      />
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+      
       <div className="text-center max-w-4xl mx-auto space-y-8 z-10 px-4">
         <div
           className={`transition-all duration-1000 delay-300 ${isVisible ? "opacity-100" : "opacity-0 translate-y-10"}`}
